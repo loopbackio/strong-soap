1ded3f7b1a42d9d91d33dc0ac60f6b31d72bb748
rashmihunt <hrashmi@us.ibm.com>
2016-09-13 16:22:31 -0700

    Fixed support for imported wsdl scenario, converted wsdl-test.js to mocha test

--- test/wsdl-test.js
+++ test/wsdl-test.js
@@ -3,133 +3,131 @@
 var fs = require('fs'),
     soap = require('..').soap,
     assert = require('assert'),
-    should = require('should');
+    should = require('should'),
+    request = require('request'),
+    http = require('http');
 
-var wsdlStrictTests = {},
-    wsdlNonStrictTests = {};
+describe('wsdl-tests', function() {
 
-fs.readdirSync(__dirname+'/wsdl/strict').forEach(function(file) {
-  if (!/.wsdl$/.exec(file)) return;
-  wsdlStrictTests['should parse and describe '+file] = function(done) {
-    soap.createClient(__dirname+'/wsdl/strict/'+file, {strict: true}, function(err, client) {
-      assert.ok(!err);
-      client.describe();
+  describe('wsdl parsing test cases', function () {
+
+    it('should parse and describe - 1 set of wsdls', function (done) {
+      fs.readdirSync(__dirname+'/wsdl/strict').forEach(function(file) {
+        if (!/.wsdl$/.exec(file)) return;
+          soap.createClient(__dirname+'/wsdl/strict/'+file, {strict: true}, function(err, client) {
+            assert.ok(!err);
+            client.describe();
+          });
+      });
       done();
     });
-  };
-});
 
-fs.readdirSync(__dirname+'/wsdl').forEach(function(file) {
-  if (!/.wsdl$/.exec(file)) return;
-  wsdlNonStrictTests['should parse and describe '+file] = function(done) {
-    soap.createClient(__dirname+'/wsdl/'+file, function(err, client) {
-      assert.ok(!err);
-      client.describe();
+
+    it('should parse and describe - 2nd set of wsdls', function (done) {
+      fs.readdirSync(__dirname+'/wsdl').forEach(function(file) {
+        if (!/.wsdl$/.exec(file)) return;
+        soap.createClient(__dirname+'/wsdl/'+file, function(err, client) {
+          assert.ok(!err);
+          client.describe();
+        });
+      });
       done();
     });
-  };
-});
 
-wsdlNonStrictTests['should not parse connection error'] = function(done) {
-  soap.createClient(__dirname+'/wsdl/connection/econnrefused.wsdl', function(err, client) {
-    assert.ok(/EADDRNOTAVAIL|ECONNREFUSED/.test(err), err);
-    done();
-  });
-};
-
-wsdlNonStrictTests['should catch parse error'] = function(done) {
-  soap.createClient(__dirname+'/wsdl/bad.txt', function(err) {
-    assert.notEqual(err, null);
-    done();
-  });
-};
-
-wsdlStrictTests['should catch parse error'] = function(done) {
-  soap.createClient(__dirname+'/wsdl/bad.txt', {strict: true}, function(err) {
-    assert.notEqual(err, null);
-    done();
-  });
-};
-
-wsdlStrictTests['should parse external wsdl'] = function(done) {
-  soap.createClient(__dirname+'/wsdl/wsdlImport/main.wsdl', {strict: true}, function(err, client){
-    assert.ok(!err);
-    assert.deepEqual(Object.keys(client.wsdl.definitions.schemas),
-      ['http://example.com/', 'http://schemas.microsoft.com/2003/10/Serialization/Arrays']);
-    assert.equal(typeof client.getLatestVersion, 'function');
-    done();
-  });
-};
-
-wsdlStrictTests['should get the parent namespace when parent namespace is empty string'] = function(done) {
-  soap.createClient(__dirname+'/wsdl/marketo.wsdl', {strict: true}, function(err, client){
-    assert.ok(!err);
-    client.getLeadChanges({
-        batchSize: 1,
-        startPosition: {activityCreatedAt: '2014-04-14T22:03:48.587Z'},
-        activityNameFilter: {stringItem: ['Send Email']}
-      }, function() {
+    it('should not parse connection error', function (done) {
+      soap.createClient(__dirname+'/wsdl/connection/econnrefused.wsdl', function(err, client) {
+        assert.ok(/EADDRNOTAVAIL|ECONNREFUSED/.test(err), err);
         done();
       });
-  });
-};
-
-wsdlStrictTests['should handle element ref'] = function(done) {
-  var expectedMsg = '<ns1:fooRq xmlns:ns1="http://example.com/bar/xsd"' +
-    ' xmlns="http://example.com/bar/xsd"><bar1:paymentRq' +
-    ' xmlns:bar1="http://example.com/bar1/xsd">' +
-    '<bar1:bankSvcRq>' +
-    '<bar1:requestUID>001</bar1:requestUID></bar1:bankSvcRq>' +
-    '</bar1:paymentRq></ns1:fooRq>';
-  soap.createClient(__dirname + '/wsdl/elementref/foo.wsdl', {strict: true}, function(err, client) {
-    assert.ok(!err);
-    client.fooOp({paymentRq: {bankSvcRq: {requestUID: '001'}}}, function(err, result) {
-      assert.equal(client.lastMessage, expectedMsg);
-      done();
     });
-  });
-};
 
-wsdlStrictTests['should handle type ref'] = function(done) {
-  var expectedMsg = require('./wsdl/typeref/request.xml.js');
-  var reqJson = require('./wsdl/typeref/request.json');
-  soap.createClient(__dirname + '/wsdl/typeref/order.wsdl', {strict: true}, function(err, client) {
-    assert.ok(!err);
-    client.order(reqJson, function(err, result) {
-      assert.equal(client.lastMessage, expectedMsg);
-      done();
+    it('should catch parse error', function (done) {
+      soap.createClient(__dirname+'/wsdl/bad.txt', function(err) {
+        assert.notEqual(err, null);
+        done();
+      });
     });
-  });
-};
 
-wsdlStrictTests['should get empty namespace prefix'] = function(done) {
-  var expectedMsg = '<ns1:fooRq xmlns:ns1="http://example.com/bar/xsd"' +
-    ' xmlns="http://example.com/bar/xsd"><bar1:paymentRq' +
-    ' xmlns:bar1="http://example.com/bar1/xsd">' +
-    '<bar1:bankSvcRq>' +
-    '<requestUID>001</requestUID></bar1:bankSvcRq>' +
-    '</bar1:paymentRq></ns1:fooRq>';
-  // var expectedMsg = 'gg';
 
-  soap.createClient(__dirname + '/wsdl/elementref/foo.wsdl', {strict: true}, function(err, client) {
-    assert.ok(!err);
-    client.fooOp({paymentRq: {bankSvcRq: {':requestUID': '001'}}}, function(err, result) {
-      assert.equal(client.lastMessage, expectedMsg);
-      done();
+    it('should parse external wsdl', function (done) {
+      soap.createClient(__dirname+'/wsdl/wsdlImport/main.wsdl', {strict: true}, function(err, client){
+        assert.ok(!err);
+        assert.deepEqual(Object.keys(client.wsdl.definitions.schemas),
+          ['http://example.com/', 'http://schemas.microsoft.com/2003/10/Serialization/Arrays']);
+        assert.equal(typeof client.getLatestVersion, 'function');
+        done();
+      });
     });
-  });
-};
 
-wsdlNonStrictTests['should load same namespace from included xsd'] = function(done) {
-  var expected = '{"DummyService":{"DummyPortType":{"Dummy":{"input":{"ID":"IdType|xs:string|pattern","Name":"NameType|xs:string|minLength,maxLength"},"output":{"Result":"dummy:DummyList"}}}}}';
-  soap.createClient(__dirname + '/wsdl/xsdinclude/xsd_include.wsdl', function(err, client) {
-    assert.ok(!err);
-    assert.equal(JSON.stringify(client.describe()), expected);
-    done();
-  });
-};
+    it('should get the parent namespace when parent namespace is empty string', function (done) {
+      soap.createClient(__dirname+'/wsdl/marketo.wsdl', {strict: true}, function(err, client){
+        assert.ok(!err);
+        client.getLeadChanges({
+          batchSize: 1,
+          startPosition: {activityCreatedAt: '2014-04-14T22:03:48.587Z'},
+          activityNameFilter: {stringItem: ['Send Email']}
+        }, function() {
+          done();
+        });
+      });
+    });
 
-module.exports = {
-  'WSDL Parser (strict)': wsdlStrictTests,
-  'WSDL Parser (non-strict)': wsdlNonStrictTests
-};
+    it('should handle element ref', function (done) {
+      var expectedMsg = '<ns1:fooRq xmlns:ns1="http://example.com/bar/xsd"' +
+        ' xmlns="http://example.com/bar/xsd"><bar1:paymentRq' +
+        ' xmlns:bar1="http://example.com/bar1/xsd">' +
+        '<bar1:bankSvcRq>' +
+        '<bar1:requestUID>001</bar1:requestUID></bar1:bankSvcRq>' +
+        '</bar1:paymentRq></ns1:fooRq>';
+      soap.createClient(__dirname + '/wsdl/elementref/foo.wsdl', {strict: true}, function(err, client) {
+        assert.ok(!err);
+        client.fooOp({paymentRq: {bankSvcRq: {requestUID: '001'}}}, function(err, result) {
+          assert.equal(client.lastMessage, expectedMsg);
+          done();
+        });
+      });
+    });
+
+    it('should handle type ref', function (done) {
+      var expectedMsg = require('./wsdl/typeref/request.xml.js');
+      var reqJson = require('./wsdl/typeref/request.json');
+      soap.createClient(__dirname + '/wsdl/typeref/order.wsdl', {strict: true}, function(err, client) {
+        assert.ok(!err);
+        client.order(reqJson, function(err, result) {
+          assert.equal(client.lastMessage, expectedMsg);
+          done();
+        });
+      });
+    });
+
+    it('should get empty namespace prefix', function (done) {
+      var expectedMsg = '<ns1:fooRq xmlns:ns1="http://example.com/bar/xsd"' +
+        ' xmlns="http://example.com/bar/xsd"><bar1:paymentRq' +
+        ' xmlns:bar1="http://example.com/bar1/xsd">' +
+        '<bar1:bankSvcRq>' +
+        '<requestUID>001</requestUID></bar1:bankSvcRq>' +
+        '</bar1:paymentRq></ns1:fooRq>';
+      // var expectedMsg = 'gg';
+
+      soap.createClient(__dirname + '/wsdl/elementref/foo.wsdl', {strict: true}, function(err, client) {
+        assert.ok(!err);
+        client.fooOp({paymentRq: {bankSvcRq: {':requestUID': '001'}}}, function(err, result) {
+          assert.equal(client.lastMessage, expectedMsg);
+          done();
+        });
+      });
+    });
+
+
+    it('should load same namespace from included xsd', function (done) {
+      var expected = '{"DummyService":{"DummyPortType":{"Dummy":{"input":{"ID":"IdType|xs:string|pattern","Name":"NameType|xs:string|minLength,maxLength"},"output":{"Result":"dummy:DummyList"}}}}}';
+      soap.createClient(__dirname + '/wsdl/xsdinclude/xsd_include.wsdl', function(err, client) {
+        assert.ok(!err);
+        assert.equal(JSON.stringify(client.describe()), expected);
+        done();
+      });
+    });
+
+  });
+});
+
