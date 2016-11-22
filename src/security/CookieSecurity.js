938d03e078e48396491e8b90b9bcae23d9ae52d8
Branden Horiuchi <bhoriuchi@gmail.com>
2016-11-22 07:46:36 -0800

    added cookie security

---
+++ src/security/CookieSecurity.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _ = require('lodash');
+var Security = require('./security');
+
+/*
+ * Accepts either a cookie or lastResponseHeaders
+ */
+class CookieSecurity extends Security {
+  constructor(cookie, options) {
+    super(options);
+
+    cookie = _.isArray(cookie) ? cookie : [cookie]
+    var cookies = _.map(_.get(cookie, 'set-cookie', cookie), function (c) {
+      return c.split(';')[0];
+    })
+
+    this.cookie = cookies.join('; ');
+  }
+
+  addHttpHeaders(headers) {
+    headers.Cookie = this.cookie;
+  }
+}
+
+module.exports = CookieSecurity;
