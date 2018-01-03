a323d16e84aecc2caee9720b917e31eef8c0d680
Raymond Feng <enjoyjava@gmail.com>
2018-01-03 09:29:17 -0800

    fix soap version detection

--- src/parser/wsdl/operation.js
+++ src/parser/wsdl/operation.js
@@ -43,12 +43,12 @@ class Operation extends WSDLElement {
         this.soapAction = child.$soapAction || '';
         this.style = child.$style || '';
         //figure out from the binding operation soap version 1.1 or 1.2
-        if (child.$soapAction !== undefined){
-          if(child.nsURI === 'http://schemas.xmlsoap.org/wsdl/soap/'){
-            this.soapVersion ='1.1';
-          } else if(child.nsURI === 'http://schemas.xmlsoap.org/wsdl/soap12/') {
-            this.soapVersion ='1.2';
-          }
+        if (child.nsURI === 'http://schemas.xmlsoap.org/wsdl/soap/') {
+          this.soapVersion ='1.1';
+        } else if(child.nsURI === 'http://schemas.xmlsoap.org/wsdl/soap12/') {
+          this.soapVersion ='1.2';
+        } else {
+          this.soapVersion = '1.1';
         }
         break;
     }
