eea475b9a89a918daff62b6ae5178857e10e05c7
biniam <biniam@ca.ibm.com>
2018-08-09 15:44:46 -0400

    test using new wsdl

--- example/json2xmlwithschema.js
+++ example/json2xmlwithschema.js
@@ -6,32 +6,58 @@ var path = require('path');
 
 var util = require('util');
 
-
-var xmlFile = fs.readFileSync(
-  path.resolve(__dirname, '..', 'cybersource instance doc_cutdown.xml'),
-  {encoding: 'utf-8'},
-);
-
-
 WSDL.open(
-  path.resolve(__dirname, '..', 'xsds', 'CyberSourceTransaction_1.51.xsd'),
+  path.resolve(__dirname, 'wsdls', 'AddressLookupSOAP2.wsdl'),
   {},
   function(err, wsdl) {
-    // var xmlHandler = new XMLHandler();
-    xmlHandler = new XMLHandler(
-      wsdl.definitions.schemas,
-      {},
-    );
+    xmlHandler = new XMLHandler(wsdl.definitions.schemas, {});
 
-    var json = xmlHandler.xmlToJson(null, xmlFile, null);
-    console.log('json ', JSON.stringify(json, null, 2));
+    /*     var json = {
+      postcodeResponse:{
+      $attributes: {
+        $xsiType: {
+          type: 'Address',
+          xmlns: 'http://apimtest.org/AddressLookup/',
+        },
+      },
+      street: 'Anywhere Street',
+      town: 'Beyond',
+      county: 'Cupboardshire',
+      country: 'UK',
+      postcode: '4321',
+      }
+    };
+
+    var node = xmlHandler.jsonToXml(
+        null,
+        null,
+        null,
+        json,
+      ); */
+
+    var json = {
+      $attributes: {
+        $xsiType: {
+          type: 'Address',
+          xmlns: 'http://apimtest.org/AddressLookup/',
+        },
+      },
+      street: 'Anywhere Street',
+      town: 'Beyond',
+      county: 'Cupboardshire',
+      country: 'UK',
+      postcode: '4321',
+    };
 
     var node = xmlHandler.jsonToXml(
       null,
       null,
-      XMLHandler.createSOAPEnvelopeDescriptor('urn:schemas-cybersource-com:transaction-data-1.51'),
+      wsdl.definitions.schemas[
+        'http://apimtest.org/AddressLookup/'
+      ].children[1].describe(wsdl.definitions),
       json,
     );
+
     var xml = node.end({pretty: true});
     console.log('xml ', xml);
   },
