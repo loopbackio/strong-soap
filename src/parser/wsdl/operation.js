40504de5bf39df2257923b1a0b0d7509869fec62
Gabriel <Gabriel.Fialho@domesticandgeneral.com>
2023-02-07 13:02:48 -0300

    fix: fault error

--- src/parser/wsdl/operation.js
+++ src/parser/wsdl/operation.js
@@ -3,25 +3,25 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-'use strict';
+"use strict";
 
-var g = require('../../globalize');
-var WSDLElement = require('./wsdlElement');
-var descriptor = require('../xsd/descriptor');
+var g = require("../../globalize");
+var WSDLElement = require("./wsdlElement");
+var descriptor = require("../xsd/descriptor");
 var ElementDescriptor = descriptor.ElementDescriptor;
 var TypeDescriptor = descriptor.TypeDescriptor;
-var QName = require('../qname');
-var helper = require('../helper');
-var SimpleType = require('../xsd/simpleType');
+var QName = require("../qname");
+var helper = require("../helper");
+var SimpleType = require("../xsd/simpleType");
 
-var assert = require('assert');
+var assert = require("assert");
 
 const Style = {
-  documentLiteralWrapped: 'documentLiteralWrapped',
-  documentLiteral: 'documentLiteral',
-  rpcLiteral: 'rpcLiteral',
-  rpcEncoded: 'rpcEncoded',
-  documentEncoded: 'documentEncoded'
+  documentLiteralWrapped: "documentLiteralWrapped",
+  documentLiteral: "documentLiteral",
+  rpcLiteral: "rpcLiteral",
+  rpcEncoded: "rpcEncoded",
+  documentEncoded: "documentEncoded",
 };
 
 class Operation extends WSDLElement {
@@ -35,26 +35,29 @@ class Operation extends WSDLElement {
 
   addChild(child) {
     switch (child.name) {
-      case 'input':
+      case "input":
         this.input = child;
         break;
-      case 'output':
+      case "output":
         this.output = child;
         break;
-      case 'fault':
+      case "fault":
         this.faults.push(child);
         break;
-      case 'operation': // soap:operation
-        this.soapAction = child.$soapAction || '';
-        this.style = child.$style || '';
-        this.soapActionRequired = child.$soapActionRequired === 'true' || child.$soapActionRequired === '1' || false;
+      case "operation": // soap:operation
+        this.soapAction = child.$soapAction || "";
+        this.style = child.$style || "";
+        this.soapActionRequired =
+          child.$soapActionRequired === "true" ||
+          child.$soapActionRequired === "1" ||
+          false;
         //figure out from the binding operation soap version 1.1 or 1.2
-        if (child.nsURI === 'http://schemas.xmlsoap.org/wsdl/soap/') {
-          this.soapVersion ='1.1';
-        } else if(child.nsURI === 'http://schemas.xmlsoap.org/wsdl/soap12/') {
-          this.soapVersion ='1.2';
+        if (child.nsURI === "http://schemas.xmlsoap.org/wsdl/soap/") {
+          this.soapVersion = "1.1";
+        } else if (child.nsURI === "http://schemas.xmlsoap.org/wsdl/soap12/") {
+          this.soapVersion = "1.2";
         } else {
-          this.soapVersion = '1.1';
+          this.soapVersion = "1.1";
         }
         break;
     }
@@ -68,7 +71,7 @@ class Operation extends WSDLElement {
       for (let i = 0, n = this.faults.length; i < n; i++) {
         this.faults[i].postProcess(definitions);
       }
-      if (this.parent.name === 'binding') {
+      if (this.parent.name === "binding") {
         this.getMode();
       }
       this._processed = true;
@@ -81,14 +84,17 @@ class Operation extends WSDLElement {
     if (param == null) return null;
     var headers = new descriptor.TypeDescriptor();
     if (!param.headers) return headers;
-    param.headers.forEach(function(header) {
+    param.headers.forEach(function (header) {
       var part = header.part;
       if (part && part.element) {
         headers.addElement(part.element.describe(definitions));
       } else if (part && part.type) {
-        g.warn('{{WS-I}} violation: ' +
-          '{{http://ws-i.org/profiles/basicprofile-1.2-2010-11-09.html#BP2113}}' +
-          ' part %s', part.$name);
+        g.warn(
+          "{{WS-I}} violation: " +
+            "{{http://ws-i.org/profiles/basicprofile-1.2-2010-11-09.html#BP2113}}" +
+            " part %s",
+          part.$name
+        );
       }
     });
     return headers;
@@ -102,9 +108,12 @@ class Operation extends WSDLElement {
       if (part && part.element) {
         faults[f] = part.element.describe(definitions);
       } else {
-        g.warn('{{WS-I}} violation: ' +
-          '{{http://ws-i.org/profiles/basicprofile-1.2-2010-11-09.html#BP2113}}' +
-          ' part %s', part.$name);
+        g.warn(
+          "{{WS-I}} violation: " +
+            "{{http://ws-i.org/profiles/basicprofile-1.2-2010-11-09.html#BP2113}}" +
+            " part %s",
+          part.$name
+        );
       }
     }
     return faults;
@@ -157,12 +166,21 @@ class Operation extends WSDLElement {
       case Style.rpcLiteral:
       case Style.rpcEncoded:
         // The operation wrapper element
-        let nsURI = (this.input && this.input.body &&
-          this.input.body.namespace) || this.targetNamespace;
+        let nsURI =
+          (this.input && this.input.body && this.input.body.namespace) ||
+          this.targetNamespace;
         input = new descriptor.ElementDescriptor(
-          new QName(nsURI, this.$name), null, 'qualified', false);
+          new QName(nsURI, this.$name),
+          null,
+          "qualified",
+          false
+        );
         output = new descriptor.ElementDescriptor(
-          new QName(nsURI, this.$name + 'Response'), null, 'qualified', false);
+          new QName(nsURI, this.$name + "Response"),
+          null,
+          "qualified",
+          false
+        );
         let inputParts = new descriptor.TypeDescriptor();
         let outputParts = new descriptor.TypeDescriptor();
         if (this.input && this.input.body) {
@@ -171,13 +189,21 @@ class Operation extends WSDLElement {
             let type;
             if (part.type) {
               if (part.type instanceof SimpleType) {
-                var qName = new QName(part.type.targetNamespace, part.type.$name, part.type.prefix);
+                var qName = new QName(
+                  part.type.targetNamespace,
+                  part.type.$name,
+                  part.type.prefix
+                );
                 type = qName;
               } else {
                 type = part.type.qname;
               }
               let element = new descriptor.ElementDescriptor(
-                new QName(nsURI, p), type, 'unqualified', false);
+                new QName(nsURI, p),
+                type,
+                "unqualified",
+                false
+              );
               inputParts.addElement(element);
             } else if (part.element) {
               var elementDescriptor = part.element.describe(definitions);
@@ -191,13 +217,21 @@ class Operation extends WSDLElement {
             let type;
             if (part.type) {
               if (part.type instanceof SimpleType) {
-                var qName = new QName(part.type.targetNamespace, part.type.$name, part.type.prefix);
+                var qName = new QName(
+                  part.type.targetNamespace,
+                  part.type.$name,
+                  part.type.prefix
+                );
                 type = qName;
               } else {
                 type = part.type.qname;
               }
               let element = new descriptor.ElementDescriptor(
-                new QName(nsURI, p), type, 'unqualified', false);
+                new QName(nsURI, p),
+                type,
+                "unqualified",
+                false
+              );
               outputParts.addElement(element);
             } else if (part.element) {
               let elementDescriptor = part.element.describe(definitions);
@@ -209,7 +243,9 @@ class Operation extends WSDLElement {
         output.elements = outputParts.elements;
         break;
       case Style.documentEncoded:
-        throw new Error(g.f('{{WSDL}} style not supported: %s', Style.documentEncoded));
+        throw new Error(
+          g.f("{{WSDL}} style not supported: %s", Style.documentEncoded)
+        );
     }
 
     let faults = this.describeFaults(definitions);
@@ -223,54 +259,85 @@ class Operation extends WSDLElement {
       soapVersion: this.soapVersion,
       input: {
         body: input,
-        headers: inputHeaders
+        headers: inputHeaders,
       },
       output: {
         body: output,
-        headers: outputHeaders
+        headers: outputHeaders,
       },
       faults: {
-          body: {Fault : {faults}}
-      }
+        body: { Fault: { faults } },
+      },
     };
-    this.descriptor.inputEnvelope =
-      Operation.createEnvelopeDescriptor(this.descriptor.input, false, this.soapVersion);
-    this.descriptor.outputEnvelope =
-      Operation.createEnvelopeDescriptor(this.descriptor.output, true, this.soapVersion);
-    this.descriptor.faultEnvelope =
-      Operation.createEnvelopeDescriptor(this.descriptor.faults, true, this.soapVersion);
+    this.descriptor.inputEnvelope = Operation.createEnvelopeDescriptor(
+      this.descriptor.input,
+      false,
+      this.soapVersion
+    );
+    this.descriptor.outputEnvelope = Operation.createEnvelopeDescriptor(
+      this.descriptor.output,
+      true,
+      this.soapVersion
+    );
+    this.descriptor.faultEnvelope = Operation.createEnvelopeDescriptor(
+      this.descriptor.faults,
+      true,
+      this.soapVersion
+    );
 
     return this.descriptor;
   }
 
-  static createEnvelopeDescriptor(parameterDescriptor, isOutput, soapVersion, prefix, nsURI) {
-    prefix = prefix || 'soap';
+  static createEnvelopeDescriptor(
+    parameterDescriptor,
+    isOutput,
+    soapVersion,
+    prefix,
+    nsURI
+  ) {
+    prefix = prefix || "soap";
     var soapNsURI;
-    if (soapVersion === '1.1') {
-      soapNsURI = 'http://schemas.xmlsoap.org/soap/envelope/';
-    } else if (soapVersion === '1.2') {
-      soapNsURI = 'http://www.w3.org/2003/05/soap-envelope';
+    if (soapVersion === "1.1") {
+      soapNsURI = "http://schemas.xmlsoap.org/soap/envelope/";
+    } else if (soapVersion === "1.2") {
+      soapNsURI = "http://www.w3.org/2003/05/soap-envelope";
     }
 
     nsURI = nsURI || soapNsURI;
     var descriptor = new TypeDescriptor();
 
     var envelopeDescriptor = new ElementDescriptor(
-      new QName(nsURI, 'Envelope', prefix), null, 'qualified', false);
+      new QName(nsURI, "Envelope", prefix),
+      null,
+      "qualified",
+      false
+    );
     descriptor.add(envelopeDescriptor);
 
     var headerDescriptor = new ElementDescriptor(
-      new QName(nsURI, 'Header', prefix), null, 'qualified', false);
+      new QName(nsURI, "Header", prefix),
+      null,
+      "qualified",
+      false
+    );
 
     var bodyDescriptor = new ElementDescriptor(
-      new QName(nsURI, 'Body', prefix), null, 'qualified', false);
+      new QName(nsURI, "Body", prefix),
+      null,
+      "qualified",
+      false
+    );
 
     envelopeDescriptor.addElement(headerDescriptor);
     envelopeDescriptor.addElement(bodyDescriptor);
 
     //add only if input or output. Fault is list of faults unlike input/output element and fault needs further processing below,
     //before it can be added to the <body>
-    if (parameterDescriptor && parameterDescriptor.body && !parameterDescriptor.body.Fault) {
+    if (
+      parameterDescriptor &&
+      parameterDescriptor.body &&
+      !parameterDescriptor.body?.Fault
+    ) {
       bodyDescriptor.add(parameterDescriptor.body);
     }
 
@@ -293,48 +360,113 @@ class Operation extends WSDLElement {
         </soap:Fault>
      </soap:Body>
      */
-    if (isOutput && parameterDescriptor && parameterDescriptor.body.Fault) {
-      let xsdStr = new QName(helper.namespaces.xsd, 'string', 'xsd');
+    if (isOutput && parameterDescriptor && parameterDescriptor.body?.Fault) {
+      let xsdStr = new QName(helper.namespaces.xsd, "string", "xsd");
       var form;
-      if (soapVersion === '1.1') {
-        form = 'unqualified';
-      } else if (soapVersion === '1.2') {
-        form = 'qualified';
+      if (soapVersion === "1.1") {
+        form = "unqualified";
+      } else if (soapVersion === "1.2") {
+        form = "qualified";
       }
 
       let faultDescriptor = new ElementDescriptor(
-        new QName(nsURI, 'Fault', prefix), null, 'qualified', false);
+        new QName(nsURI, "Fault", prefix),
+        null,
+        "qualified",
+        false
+      );
       bodyDescriptor.add(faultDescriptor);
       let detailDescriptor;
-      if (soapVersion === '1.1') {
+      if (soapVersion === "1.1") {
         faultDescriptor.add(
-          new ElementDescriptor(new QName(nsURI, 'faultcode', prefix), null, form, false));
+          new ElementDescriptor(
+            new QName(nsURI, "faultcode", prefix),
+            null,
+            form,
+            false
+          )
+        );
         faultDescriptor.add(
-          new ElementDescriptor(new QName(nsURI, 'faultstring', prefix), null, form, false));
+          new ElementDescriptor(
+            new QName(nsURI, "faultstring", prefix),
+            null,
+            form,
+            false
+          )
+        );
         faultDescriptor.add(
-          new ElementDescriptor(new QName(nsURI, 'faultactor', prefix), null, form, false));
-        detailDescriptor =
-          new ElementDescriptor(new QName(nsURI, 'detail', prefix), null, form, false);
+          new ElementDescriptor(
+            new QName(nsURI, "faultactor", prefix),
+            null,
+            form,
+            false
+          )
+        );
+        detailDescriptor = new ElementDescriptor(
+          new QName(nsURI, "detail", prefix),
+          null,
+          form,
+          false
+        );
         faultDescriptor.add(detailDescriptor);
-      } else if (soapVersion === '1.2') {
-        let code = new ElementDescriptor(new QName(nsURI, 'Code', prefix));
+      } else if (soapVersion === "1.2") {
+        let code = new ElementDescriptor(new QName(nsURI, "Code", prefix));
         code.add(
-          new ElementDescriptor(new QName(nsURI, 'Value', prefix), null, form, false));
-        let subCode = new ElementDescriptor(new QName(nsURI, 'Subcode', prefix), null, form, false);
-        code.add (subCode);
+          new ElementDescriptor(
+            new QName(nsURI, "Value", prefix),
+            null,
+            form,
+            false
+          )
+        );
+        let subCode = new ElementDescriptor(
+          new QName(nsURI, "Subcode", prefix),
+          null,
+          form,
+          false
+        );
+        code.add(subCode);
         subCode.add(
-          new ElementDescriptor(new QName(nsURI, 'Value', prefix), null, form, false));
+          new ElementDescriptor(
+            new QName(nsURI, "Value", prefix),
+            null,
+            form,
+            false
+          )
+        );
         faultDescriptor.add(code, null, form, false);
-        let reason = new ElementDescriptor(new QName(nsURI, 'Reason', prefix));
+        let reason = new ElementDescriptor(new QName(nsURI, "Reason", prefix));
         reason.add(
-          new ElementDescriptor(new QName(nsURI, 'Text', prefix), null, form, false));
+          new ElementDescriptor(
+            new QName(nsURI, "Text", prefix),
+            null,
+            form,
+            false
+          )
+        );
         faultDescriptor.add(reason, null, form, false);
         faultDescriptor.add(
-          new ElementDescriptor(new QName(nsURI, 'Node', prefix), null, form, false));
+          new ElementDescriptor(
+            new QName(nsURI, "Node", prefix),
+            null,
+            form,
+            false
+          )
+        );
         faultDescriptor.add(
-          new ElementDescriptor(new QName(nsURI, 'Role', prefix), null, form, false));
-        detailDescriptor =
-          new ElementDescriptor(new QName(nsURI, 'Detail', prefix), null, form, false);
+          new ElementDescriptor(
+            new QName(nsURI, "Role", prefix),
+            null,
+            form,
+            false
+          )
+        );
+        detailDescriptor = new ElementDescriptor(
+          new QName(nsURI, "Detail", prefix),
+          null,
+          form,
+          false
+        );
         faultDescriptor.add(detailDescriptor);
       }
       //multiple faults may be defined in wsdl for this operation. Go though every Fault and add it under <detail> element.
@@ -347,8 +479,9 @@ class Operation extends WSDLElement {
   }
 
   getMode() {
-    let use = this.input && this.input.body && this.input.body.use || 'literal';
-    if (this.style === 'document' && use === 'literal') {
+    let use =
+      (this.input && this.input.body && this.input.body.use) || "literal";
+    if (this.style === "document" && use === "literal") {
       // document literal
       let element = null;
       let count = 0;
@@ -357,8 +490,8 @@ class Operation extends WSDLElement {
           let part = this.input.body.parts[p];
           element = part.element;
           if (!(part.element && !part.type)) {
-            console.error('Document/literal part should use element', part);
-            throw new Error('Document/literal part should use element');
+            console.error("Document/literal part should use element", part);
+            throw new Error("Document/literal part should use element");
           }
           count++;
         }
@@ -371,8 +504,10 @@ class Operation extends WSDLElement {
           for (let p in this.output.body.parts) {
             let part = this.output.body.parts[p];
             element = part.element;
-            assert(part.element && !part.type,
-              'Document/literal part should use element');
+            assert(
+              part.element && !part.type,
+              "Document/literal part should use element"
+            );
             count++;
           }
         }
@@ -384,22 +519,25 @@ class Operation extends WSDLElement {
       } else {
         this.mode = Style.documentLiteral;
       }
-    } else if (this.style === 'document' && use === 'encoded') {
+    } else if (this.style === "document" && use === "encoded") {
       this.mode = Style.documentEncoded;
-    } else if (this.style === 'rpc' && use === 'encoded') {
+    } else if (this.style === "rpc" && use === "encoded") {
       this.mode = Style.rpcEncoded;
-    } else if (this.style === 'rpc' && use === 'literal') {
+    } else if (this.style === "rpc" && use === "literal") {
       this.mode = Style.rpcLiteral;
     }
     return this.mode;
   }
-
 }
 
 Operation.Style = Style;
-Operation.elementName = 'operation';
-Operation.allowedChildren = ['documentation', 'input', 'output', 'fault',
-  'operation'];
+Operation.elementName = "operation";
+Operation.allowedChildren = [
+  "documentation",
+  "input",
+  "output",
+  "fault",
+  "operation",
+];
 
 module.exports = Operation;
-
