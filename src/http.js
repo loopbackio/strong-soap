9a50862486118d8bba62732a285513f194b90ee9
Todd Tarsi <tarsitodd@gmail.com>
2024-03-17 15:25:53 +0000

    chore: switch to httpntlm and fix flaky test
    Signed-off-by: Todd Tarsi <tarsitodd@gmail.com>

--- src/http.js
+++ src/http.js
@@ -9,7 +9,7 @@ var url = require('url');
 var requestModule = require('@cypress/request');
 var debug = require('debug')('strong-soap:http');
 var debugSensitive = require('debug')('strong-soap:http:sensitive');
-var httpntlm = require('httpntlm-maa');
+var httpntlm = require('httpntlm');
 var uuid = require('uuid').v4;
 
 
